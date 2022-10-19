80f24d030c74e21b9df4a30ab205ee25ecd41474
Alison <alisonvieira.av29@gmail.com>
2022-10-19 14:21:45 -0300

    declare variables, operations_id for used in foreach

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ number1Bool = false;
 number2Bool = false;
 ultimaOp = "";
 result = 0; 
+
+window.onload = () =>{
+
+    let display = document.getElementById("display");
+    let displayCount = document.getElementById("displayCount");
+
+    operations_id = [ 
+        [".","ponto"], ["+","soma"], ["=","equal"], 
+        ["*","mult"], ["-","minus"], ["/","div"],
+        ['eraser',"eraser"], ["C","C"], ["-1","-1"],
+        ["x2","x2"], ["%","percentual"], ["xX", "xX"]
+    ];
+
+        
+}
